37c3a9b6bdf9a7e48c4a86e29411c11ddb4c0304
rhbekti <rhbekti.ac.id@gmail.com>
2023-07-13 05:55:05 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  await server.start();
+
+  server.route(routes);
+
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
